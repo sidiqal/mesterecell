a6e7c03d5823de232683f98a27383cc46c7cca32
inoiz32 <sidiqtamvan@gmail.com>
2020-11-18 22:12:10 +0700

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: false
+})
+
+// Hero
+sr.reveal('.hero-img', {delay: 400})
+sr.reveal('.hero-title', {delay: 300})
+sr.reveal('.hero-subtitle', {delay: 200})
+
+// Feature
+sr.reveal('.feature-title', {})
+sr.reveal('.card-1', {delay: 100})
+sr.reveal('.card-2', {delay: 200})
+sr.reveal('.card-3', {delay: 300})
+sr.reveal('.card-4', {delay: 400})
+
+// Excellence
+sr.reveal('.excellence-title', {})
+sr.reveal('.item-1', {delay: 100})
+sr.reveal('.item-2', {delay: 150})
+sr.reveal('.item-3', {delay: 200})
+sr.reveal('.item-4', {delay: 250})
+sr.reveal('.item-5', {delay: 300})
+sr.reveal('.excellence-img', {delay: 400})
+
+// Kenawhy
+sr.reveal('.kenawhy-title', {})
+
